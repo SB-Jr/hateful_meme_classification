1a59eda7ff960b50fc631f2a76000da055f79df2
Shrijit Basak <sbasak33@gatech.edu>
2025-03-25 00:18:47 +0530

    initial commit with torch dataset

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hateful Meme Classification
+
+## Task
+
+Multiple memes are hateful due to certain reasons. We classify if a meme is hateful or not.
+
+
+## Dataset
+
+1. FB Hateful Meme Classification - https://www.kaggle.com/datasets/parthplc/facebook-hateful-meme-dataset/data
---
+++ src/utils/hmc_dataset.py
@@ -0,0 +1,33 @@
+import torch
+import json
+import os
+from PIL import Image
+
+class HatefulMemesDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, img_only=True, limit_to_examples=None):
+        self.data = [json.loads(l) for l in open(data_path)]
+        if limit_to_examples & limit_to_examples > 0:
+            self.data = self.data[:limit_to_examples]
+        self.data_dir = os.path.dirname(data_path)
+        self.img_only = img_only
+            
+    def __getitem__(self, index: int):
+        # Load images on the fly.
+        image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
+        text = self.data[index]["text"]
+        label = self.data[index]["label"]
+        if self.img_only:
+            return image, label
+        else:
+            return image, text, label
+
+    def load_image_only(self, index: int):
+        image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
+        return image
+    
+    def get_label(self, index: int):
+        label = self.data[index]["label"]
+        return label
+    
+    def __len__(self):
+        return len(self.data)
